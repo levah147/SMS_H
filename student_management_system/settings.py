bf7deffaa0642946ac1e4f2aae3ee0dc931ba898
Levah <emmanuelnater@gmail.com>
2025-02-19 12:53:10 +0100

    database

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -103,17 +103,17 @@ import dj_database_url
 
 
 DATABASES = {
-    'default': dj_database_url.parse(env('DATABASE_URL1'))
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'django',
