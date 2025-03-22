bbd7f6f947f7779467d8cd214c00031faef0bf28
Levah <emmanuelnater@gmail.com>
2025-03-22 20:08:34 +0100

    pip freeze > requirements.txt

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -93,13 +93,27 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-import environ
+from  decouple import config
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':config("DB_NAME"),
+        'USER' :config("DB_USER"),
+        'PASSWORD' :config("DB_PASSWORD"),
+        'HOST' :config("DB_HOST"),
+        'PORT' :config("DB_PORT"),
+    }
+}
 
 
-env = environ.Env()
+# import environ
 
-environ.Env.read_env()
-import dj_database_url
+
+# env = environ.Env()
+
+# environ.Env.read_env()
+# import dj_database_url
 
 
 # DATABASES = {
@@ -108,12 +122,12 @@ import dj_database_url
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
     # 'default': {
     #     'ENGINE': 'django.db.backends.mysql',
     #     'NAME': 'django',
